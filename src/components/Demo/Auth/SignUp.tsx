dad1428b2eb506b912901f1a628e53e9ac1b3501
akbenngold <akbenngold@gmail.com>
2024-08-16 14:41:42 +0100

    bug fixes

--- src/components/Demo/Auth/SignUp.tsx
+++ src/components/Demo/Auth/SignUp.tsx
@@ -70,8 +70,7 @@ const SignUp: React.FC<SignUpProps> = ({ setSignReq, setModal }) => {
     <div className="size mt-[6rem] text-center">
       <h2 className="text-3xl">Sign up with email</h2>
       <p className="w-full sm:w-[25rem] mx-auto py-[3rem]">
-        Enter the email address associated with your account, and we’ll send a
-        magic link to your inbox.
+        We are eager to have you join
       </p>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <Input form={form} setForm={setForm} type="text" title="username" />
