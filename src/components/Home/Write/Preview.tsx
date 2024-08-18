77aff3b6a8155730d7675b5ad60c61f46329be32
Ojima <72498960+akbenngold@users.noreply.github.com>
2024-08-18 09:19:25 +0100

    Update Preview.tsx

--- src/components/Home/Write/Preview.tsx
+++ src/components/Home/Write/Preview.tsx
@@ -155,7 +155,7 @@ const Preview: React.FC<PreviewProps> = ({
           <div className="flex-[1] flex flex-col gap-4 mb-5 md:mb-0">
             <h3 className="text-2xl">
               Publishing to:
-              <span className="font-bold capitalize">Milad Tech</span>
+              <span className="font-bold capitalize">Chatter</span>
             </h3>
             <p>
               Add or change topics up to 5 so readers know what your story is
